30556e90a68e91d41836ab4c11f5685429cc4f85
Owen Herndon <owenherndon@Owens-MacBook-Pro.local>
2016-11-07 21:03:32 -0600

    edit seeder to include our logins

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,63 @@
 "use strict";
+
+let apiKeys = {};
+let uid = "";
+
 $(document).ready(function(){
-  console.log("Hello!");
+//console.log("ready");
+FbAPI.firebaseCredentials().then(function(keys){
+	//console.log("keys", keys);
+		apiKeys = keys;
+		firebase.initializeApp(apiKeys);
+	});
+
+//register
+	$('#registerButton').on('click', function(){
+		let email = $('#inputEmail').val();
+		let password = $('#inputPassword').val();
+		let username = $('#inputUsername').val();
+		let user = {
+			"email": email,
+			"password": password
+		};
+		FbAPI.registerUser(user).then(function(registerResponse){
+			console.log("reg response",registerResponse);
+			let newUser = {
+				"username":username,
+				"uid":registerResponse.uid
+			};
+			return FbAPI.addUser(apiKeys,newUser);
+			//return FbAPI.loginUser(user);
+		}).then(function(userResponse){
+			return FbAPI.loginUser(user);
+		}).then(function(loginResponse){
+			console.log("loginResponse", loginResponse);
+			uid = loginResponse.uid;
+			//createLoginButton();
+			//putTodoInDom();
+			$('#login-container').addClass('hide');
+			$('#stored-movies').removeClass('hide');
+		});
+	});
+
+
+	//login
+	$('#loginButton').on('click' , function(){
+		let email = $('#inputEmail').val();
+		let password = $('#inputPassword').val();
+
+		let user = {
+			"email": email,
+			"password": password
+		};
+		FbAPI.loginUser(user).then(function(loginResponse){
+			console.log("loginResponse", loginResponse);
+			uid = loginResponse.uid;
+			//createLoginButton();
+			//putTodoInDom();
+			$('#login-container').addClass('hide');
+			$('#stored-movies').removeClass('hide');
+		});
+	});
+
 });
