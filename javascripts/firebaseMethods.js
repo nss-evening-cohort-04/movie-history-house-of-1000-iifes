bd011301f6717b05119e3e2d2d9f5f23ed861a05
jkillz2020 <kilgore.josh@gmail.com>
2016-11-08 18:50:51 -0600

    added fb methodspost edit and delete

--- javascripts/firebaseMethods.js
+++ javascripts/firebaseMethods.js
@@ -1,2 +1,53 @@
 'use strict';
 
+var FbAPI =(function(oldFirebase){
+
+oldFirebase.addMovie = function(apiKeys, newMovie){
+    return new Promise((resolve, reject)=>{
+      $.ajax({
+        method: "POST",
+        url: `${apiKeys.databaseURL}/movies.json`,
+        data: JSON.stringify(newMovie),
+        dataType: 'json'
+      }).then((response)=>{
+        console.log("response from POST", response);
+        resolve(response);
+      }, (error)=>{
+        reject(error);
+      });
+    });
+  };
+
+  oldFirebase.deleteMovie = function(apiKeys, movieId){ 
+    return new Promise((resolve, reject)=>{
+      $.ajax({
+        method: "DELETE",
+        url: `${apiKeys.databaseURL}/movies/${movieId}.json`,
+      }).then((response)=>{
+        console.log("response from DELETE", response);
+        resolve(response);
+      }, (error)=>{
+        reject(error);
+      });
+    });
+  };
+
+  oldFirebase.editMovie = function(apiKeys, movieId, editedMovie){
+    return new Promise((resolve, reject)=>{
+      $.ajax({
+        method: "PUT",
+        url: `${apiKeys.databaseURL}/movies/${movieId}.json`,
+        data: JSON.stringify(editedMovie),
+        dataType: 'json'
+      }).then((response)=>{
+        console.log("response from PUT", response);
+        resolve(response);
+      }, (error)=>{
+        reject(error);
+      });
+    });
+  };
+
+  return oldFirebase;
+})(FbAPI || {});
+
